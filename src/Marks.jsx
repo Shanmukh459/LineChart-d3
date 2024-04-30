f4ccae2e2a96faf3aa7fd9af9b0d0e96d467e32c
Shanmukh459 <saikrrish459@gmail.com>
2024-04-30 10:48:38 -0700

    scatter plot to line chart

--- src/Marks.jsx
+++ src/Marks.jsx
@@ -1,11 +1,29 @@
-export const Marks = ({ xScale, yScale, xValue, yValue, data, tooltipFormat }) => 
-data.map((d) => (
-  <circle 
-    className="marks"
-    cx={xScale(xValue(d))}
-    cy={yScale(yValue(d))}
-    r={5}
-  >
-    <title>{tooltipFormat(xValue(d))}</title>
-  </circle>
-))
+import { line, curveNatural } from "d3";
+
+export const Marks = ({
+  xScale,
+  yScale,
+  xValue,
+  yValue,
+  data,
+  tooltipFormat,
+}) => (
+  <g className="marks">
+    <path
+      d={line()
+        .x((d) => xScale(xValue(d)))
+        .y((d) => yScale(yValue(d)))
+        (data)}
+    />
+    {data.map((d) => (
+      <circle
+        cx={xScale(xValue(d))}
+        cy={yScale(yValue(d))}
+        r={2}
+      >
+        <title>{tooltipFormat(xValue(d))}</title>
+      </circle>
+    ))}
+    ;
+  </g>
+);
